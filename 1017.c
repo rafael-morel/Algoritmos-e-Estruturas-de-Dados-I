8f09f301f8bb9f4072c813c34e7110b2e6d5fcf7
Rafael Morel <carvalho.morel26@gmail.com>
2025-02-04 13:31:08 -0300

    Create 1017.c

---
+++ 1017.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int tempo, velocidade;
+    double litros;
+    
+    scanf("%d %d", &tempo, &velocidade);
+    
+    litros = (tempo * velocidade) / 12.0;
+    
+    printf("%.3lf\n", litros);
+    
+    return 0;
+}
