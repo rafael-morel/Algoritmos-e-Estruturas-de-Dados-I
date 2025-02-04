8dae82b93ce32a5f5f862a5ab23a73d0fad4dd89
Rafael Morel <carvalho.morel26@gmail.com>
2025-02-04 13:44:04 -0300

    Create 1865.c

---
+++ 1865.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    int C;
+    scanf("%d", &C);
+    
+    while (C--) {
+        char name[50];
+        int N;
+        scanf("%s %d", name, &N);
+        if (strcmp(name, "Thor") == 0) {
+            printf("Y\n");
+        } else {
+            printf("N\n");
+        }
+    }
+    
+    return 0;
+}
